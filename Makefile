8853401097757cc1be7c7b09713d60b1ce66ae61
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-06-14 20:54:22 -0400

    Switch to build script

--- Makefile
+++
@@ -1,6 +0,0 @@
-CC      ?= gcc
-CFLAGS  =  -Wall -Wextra -Wswitch-enum -pedantic -Werror
-
-SOURCES = tlpin.c
-
-tlpin: $(SOURCES)
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Display commands, error on unset variable.
+set -xu
+
+
+
+CC=${CC:-cc}
+CFLAGS=${CFLAGS:--Wall -Wextra -Wswitch-enum -Werror -pedantic}
+
+SOURCE=tlpin.c
+EXECUTABLE=${SOURCE%.c}
+
+"$CC" $CFLAGS "$SOURCE" -o "$EXECUTABLE" || exit 1
